ce50795c0c689cb777b0ed02fd009b342c868f1b
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2022-12-08 16:02:59 +0100

    PlaywrightSessionTest with tracing

--- src/test/java/test/PlaywrightSessionTest.java
+++ src/test/java/test/PlaywrightSessionTest.java
@@ -1,53 +1,67 @@
 package test;
 
 import com.microsoft.playwright.*;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
 import java.nio.file.Paths;
 
 public class PlaywrightSessionTest {
+    // Shared between all tests in this class.
+    static Playwright playwright;
+    static Browser browser;
 
-    @Before
-    public void setUp() {
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
 
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+
+        // Start tracing before creating / navigating a page.
+        context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true));
+
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        // Stop tracing and export it into a zip archive.
+        context.tracing().stop(new Tracing.StopOptions()
+                .setPath(Paths.get("trace.zip")));
+
+        context.close();
     }
 
     @Test
     public void TestSession() {
         firstScript();
-        traceViewer();
+        secondScript();
     }
     @Test
     public void firstScript() {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(5000));
-            Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
-            System.out.println(page.title());
-        }
+        page.navigate("http://playwright.dev");
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        System.out.println(page.title());
     }
 
     @Test
-    public void traceViewer() {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(5000));
-            BrowserContext context = browser.newContext();
-
-            // Start tracing before creating / navigating a page.
-            context.tracing().start(new Tracing.StartOptions()
-                    .setScreenshots(true)
-                    .setSnapshots(true)
-                    .setSources(true));
-
-            Page page = context.newPage();
-            page.navigate("https://playwright.dev");
-
-            // Stop tracing and export it into a zip archive.
-            context.tracing().stop(new Tracing.StopOptions()
-                    .setPath(Paths.get("trace.zip")));    }
+    public void secondScript() {
+        page.navigate("https://playwright.dev");
     }
-
 
 }
