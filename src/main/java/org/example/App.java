b54afa9690f5f1137ac7e48c5ed2c03680729f4d
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2023-01-18 09:21:21 +0100

    Playwright DataConfigTest Test Main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,8 +4,11 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import session.*;
 
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.SortedMap;
 
 /**
  * Hello world!
@@ -22,5 +25,30 @@ public class App
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
             System.out.println(page.title());
         }
+
+        // Create DataConfig
+        // final DataConfig dataConfig = createDataConfig();
+        final DataConfig dataConfig = BaseFunctions.readDataConfig("DataConfigTest.json");
+
+        // Execute DataConfig
+        WebclientSession ws = new WebclientSession(dataConfig.getEloSolutionArchiveData());
+        ws.login(dataConfig.getLoginData());
+
+        for (SortedMap.Entry<Integer, ELOAction> entryEloAction: dataConfig.getEloActionData().getEloActions().entrySet()) {
+            ELOAction eloAction = entryEloAction.getValue();
+            String actionName = eloAction.getActionName();
+            Map<String, TabPage> tabPages = eloAction.getTabPages();
+
+            // Get Action Definition
+            ELOActionDef eloActionDef = dataConfig.getEloActionDefData().getEloActionDefs().get(actionName);
+
+            // Execute Action
+            ws.selectSolutionsFolder();
+            ws.executeAction(actionName, tabPages, dataConfig.getEloActionFormularData(), eloActionDef);
+        }
+
+        ws.getPage().pause();
+        ws.close();
+
     }
 }
